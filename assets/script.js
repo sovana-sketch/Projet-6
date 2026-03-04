ef80292d3bb3ac798325dcb911e92c84a0833a21
sovana-sketch <sovanahnpgg@gmail.com>
2026-03-04 19:47:40 +0100

    git commit -m "Update v2"

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,3 @@
-function previous() {
-const widthSlider = document.querySelector('.bannner') .offsetWidth;
-	document.querySelector('.bannner-img').scrollLeft -=widthSlider;
-}
-function next() {
-	const widthSlider = document.querySelector('.bannner') .offsetWidth;
-	document.querySelector('.bannner-img').scrollLeft +=widthSlider;
-	
-}
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -24,4 +15,59 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
+
+let i = 0;
+
+const arrowLeft = document.querySelector('#arrow-left')
+console.log(arrowLeft);
+const arrowRight = document.querySelector('#arrow-right')
+const bannerImg = document.querySelector('#banner-img img')
+const bannerText = document.querySelector('#banner-img p')
+
+
+arrowLeft.addEventListener('click', function(event){
+	console.log(event);
+	i = (i-1 + slides.length) % slides.length;
+	bannerImg.src = "./assets/images/slideshow/" + slides[i].image
+	bannerText.innerHTML = slides[i].tagLine
+})
+
+
+arrowRight.addEventListener('click', function(event){
+	console.log(event);
+	i = (i+1) % slides.length;
+	bannerImg.src = "./assets/images/slideshow/" + slides[i].image
+	bannerText.innerHTML = slides[i].tagLine
+})
+const p = document.querySelector('#banner-img p');
+p.style.display = "flex";
+p.style.justifyContent = "center";
+p.style.alignItems = "center";
+p.style.textAlign = "center";
+//dots
+
+function createdots() {
+  slides.forEach((_, i) => {
+    const dot = document.createElement("span");
+    dot.classList.add("dots");
+
+    dot.addEventListener("click", () => {
+      index = i;
+      showSlide();
+    });
+
+    dotsContainer.appendChild(dot);
+  });
+}
+
+// 🔹 Mise à jour visuelle des dots
+function updatedots() {
+  const allDots = document.querySelectorAll(".dot");
+
+  allDots.forEach((dot, i) => {
+    dot.classList.toggle("active", i === index);
+  });
+}
+createdots();
+showSlide();
